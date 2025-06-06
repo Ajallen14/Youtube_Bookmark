65c4fec8f726b95b3fbc58f716300cf76f471735
Allen Jude <ajallen142001@gamil.com>
2025-06-06 21:33:48 +0530

    Changes made

--- Background.js
+++ Background.js
@@ -1,11 +1,19 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-        const queryParameters = tab.url.split("?")[1];
-        const urlParameters = new URLSearchParams(queryParameters);
     
-        chrome.tabs.sendMessage(tabId, {
-            type: "NEW",
-            videoId: urlParameters.get("v")
-        });
+    if (changeInfo.status === "complete" && tab.url && tab.url.includes("youtube.com/watch")) {
+        try {
+            const queryParameters = tab.url.split("?")[1];
+            const urlParameters = new URLSearchParams(queryParameters);
+            const videoId = urlParameters.get("v");
+
+            chrome.tabs.sendMessage(tabId, {
+                type: "NEW",
+                videoId: videoId
+            }).catch((err) => {
+                console.warn("Content script not ready yet:", err);
+            });
+        } catch (error) {
+            console.error("Failed to send message to content script:", error);
+        }
     }
 });
