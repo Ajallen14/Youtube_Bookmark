f9f77f234bd8fcc1f3fe21f52914eba6cc808be8
Allen Jude <ajallen142001@gamil.com>
2025-06-06 20:40:11 +0530

    Changes made

--- Content.js
+++ Content.js
@@ -12,8 +12,17 @@
         }
     });
 
-    const newVideoLoaded = () => {
+    const fetchBookmarks = () => {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get([currentVideo], (obj) => {
+                resolve(obj[currentVideo] ? JASON.parse(obj[currentVideo]) : []);
+            });
+        });
+    }
+
+    const newVideoLoaded = async() => {
         const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0]
+        currentVideoBookmarks = await fetchBookmarks();
 
         if(!bookmarkBtnExists){
             const bookmarkBtn = document.createElement("img");
@@ -32,7 +41,7 @@
 
     newVideoLoaded();
 
-    const addNewBookmarkEventHandler = () => {
+    const addNewBookmarkEventHandler = async() => {
         const currentTime =  youtubePlayer.currentTime;
         const newBookmark = {
             time : currentTime,
@@ -40,8 +49,10 @@
             
         };
 
+        currentVideoBookmarks = await fetchBookmarks();
+
         chrome.storage.sync.set({
-            [currentVideo] : JSON.stringify([...currentVideoBookmarks, newBookmark]. sort((a, b) => a.time -b))
+            [currentVideo] : JSON.stringify([...currentVideoBookmarks, newBookmark]. sort((a, b) => a.time -b.time))
         });
     }
 })();
